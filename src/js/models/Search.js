bc5411f345590259b1384cf80d9f8f6d84388ded
bjscatena <7HwmqRmTj3uEeCYCV9mM>
2019-03-26 17:27:45 -0300

    begginning to code the functionalities

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const key = "e98feb7b629f722b2d56ccf3c7588cd5";
+
+    try {
+      const res = await axios(
+        `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      //console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
